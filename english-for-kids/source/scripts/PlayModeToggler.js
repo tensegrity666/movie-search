fd00ed92cf4bca5d714256db9aad9d04ba45a31e
Roman Yakovlev <tensegrity666@icloud.com>
2020-04-17 23:21:36 +0300

    feat: add area for answers counter

--- english-for-kids/source/scripts/PlayModeToggler.js
+++ english-for-kids/source/scripts/PlayModeToggler.js
@@ -1,7 +1,7 @@
 const togglerLabel = document.querySelector('.toggler__description-train');
 const togglerOuter = document.querySelector('.toggler');
 const button = document.querySelector('.start');
-const card = document.querySelector('.item');
+const answers = document.querySelector('.answers');
 
 export default function playModeToggler(isTrain) {
   if (isTrain) {
@@ -9,13 +9,14 @@ export default function playModeToggler(isTrain) {
     togglerOuter.classList.add('toggler_play');
     button.classList.add('start_show');
     button.setAttribute('disabled', true);
+    answers.classList.add('answers_show');
     togglerLabel.innerText = 'play';
   } else {
     togglerLabel.classList.remove('toggler__description-play');
     togglerOuter.classList.remove('toggler_play');
     button.classList.remove('start_show');
-    cards.forEach((card) => card.classList.remove('item_play'));
     button.setAttribute('disabled', false);
+    answers.classList.remove('answers_show');
     togglerLabel.innerText = 'train';
   }
 }
