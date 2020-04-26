47f97bb07b081d353f371d708bf8e23f845f3ddb
Roman Yakovlev <tensegrity666@icloud.com>
2020-04-26 21:21:01 +0300

    fix: some fixes in menu

--- english-for-kids/src/scripts/index.js
+++ english-for-kids/src/scripts/index.js
@@ -2,14 +2,14 @@
 
 import '../styles/main.scss';
 
-import sideMenuToggle from './showSideMenu';
-import togglePageType from './togglePageMode';
+import sideMenuToggle from './view/showSideMenu';
+import togglePageType from './view/togglePageMode';
 import addHashesToAddresline from './utils/routing';
 import cardsToRender from './rendering/renderTrainingCards';
 import reverseCard from './reverseCard';
 import speakWord from './speakWord';
 import goToClickedCategory from './goToCategory';
-import startPlaying from './gameLogic/PlayingProcess';
+import startPlaying from './gameLogic/startGame';
 import state from './state/state';
 
 cardsToRender(state.currentLocation);
