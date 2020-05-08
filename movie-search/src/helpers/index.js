1bfe4194209c69a8d4b6cca19d72f1abb5ef3598
Roman Yakovlev <tensegrity666@eicloud.com>
2020-05-08 19:33:16 +0300

    feat: add spinner while loading

--- movie-search/src/helpers/index.js
+++ movie-search/src/helpers/index.js
@@ -1,3 +1,16 @@
+import { THRESHOLD } from '../constants';
+
+
+function showSpinner(state) {
+  const spinner = document.querySelector('#spinner');
+
+  spinner.classList.remove('lds-facebook');
+  if (state.isLoading) {
+    spinner.classList.add('lds-facebook');
+  }
+}
+
+
 function modifyRequestText(request) {
   return request
     .toLowerCase()
@@ -7,9 +20,27 @@ function modifyRequestText(request) {
 }
 
 
-function sendRequest(url) {
+function getMoviesData(url) {
   return fetch(url)
     .then((response) => response.json());
 }
 
-export { modifyRequestText, sendRequest };
+
+function InitLoadingNextPage() {
+  let counter = 1;
+  let PAGE_NUMBER = `&page=${counter}`;
+
+  const lastMovieCardCoordinates = document.querySelector('.cardlist').lastChild.getBoundingClientRect().right;
+  const wrapperCoordinates = document.querySelector('.swiper-outer').getBoundingClientRect().right;
+
+  if (Math.floor(lastMovieCardCoordinates) - Math.floor(wrapperCoordinates) <= THRESHOLD) {
+    counter += 1;
+    PAGE_NUMBER = `&page=${counter}`;
+  }
+
+  return PAGE_NUMBER;
+}
+
+export {
+  modifyRequestText, getMoviesData, showSpinner, InitLoadingNextPage,
+};
