3028f8c6c7f09406c1d704dc958cc787605c5804
Roman Yakovlev <tensegrity666@eicloud.com>
2020-05-08 19:32:36 +0300

    feat: add mocha and testsuit

---
+++ movie-search/src/components/Searcher/index.test.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+
+const assert = require('assert');
+
+function modifyRequestText(request) {
+  return request
+    .toLowerCase()
+    .replace(/\s+/g, ' ')
+    .trim().split(' ')
+    .join('+');
+}
+
+
+describe('Запрос', () => {
+  describe('Возвращает модифицированный запрос:', () => {
+    it('возвращает переданное слово без изменений', () => {
+      assert.equal(modifyRequestText('qwerty'), 'qwerty');
+    });
+    it('возвращает несколько слов с разделителем +', () => {
+      assert.equal(modifyRequestText('qwerty qwerty'), 'qwerty+qwerty');
+    });
+    it('приводит все символы к нижнему регистру', () => {
+      assert.equal(modifyRequestText('QweRty'), 'qwerty');
+    });
+    it('yдаляет лишние пробелы в запросе', () => {
+      assert.equal(modifyRequestText(' qwe rt   wer ew eee   ee '), 'qwe+rt+wer+ew+eee+ee');
+    });
+  });
+});
